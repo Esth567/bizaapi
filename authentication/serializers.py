7f200042ff13d253af3435c78b19f28c6253e790
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-09-29 15:27:27 +0100

    fixed indentation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,22 +1,17 @@
-
 from rest_framework import serializers
 from .models import User
 
+
 class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "password")
 
-  class Meta:
-  model = User
-  fields = ( 'username', 'first_name', 'last_name', 'email', 'password' )
+    def validate(self, attrs):
+        email = attrs.get("email")
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({"email": ("Email is already in use")})
+        return super().validate(attrs)
 
-
-
-
-def validate(self, attrs):
-  email = attrs.get('email')
-  if User.objects.filter(email=email).exists():
-      raise serializers.ValidationError(
-          {'email': ('Email is already in use')})
-  return super().validate(attrs)
-
-def created(self, validated_data):
-    return User.objects.create_user(**validated_data)
+    def created(self, validated_data):
+        return User.objects.create_user(**validated_data)
